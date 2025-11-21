220590dc813797ec392bc4f81aea5a61a7faa64a
Hector <hectorvidalsoto@gmail.com>
2025-11-21 12:11:11 +0100

    añadido println desde develop

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public static void main(String[] args) {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     System.out.println("Hello and welcome!");
+    System.out.println("println desde develop");
 
     for (int i = 1; i <= 5; i++) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
